02d66b03d83ee8b502c18e5616c399a101653340
minseonkim <minseonallykim@gmail.com>
2023-10-16 11:26:25 +0900

    ,

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import logo from './logo.svg';
+import React,{useState} from "react";
 import './App.css';
-
+import TodoBoard from "./components/TodoBoard";
+/*
+1. 인풋창이 있고 버튼이 있다.
+2. 인풋창에 값을 입력하고 버튼을 클릭하면 아이템이 추가된다.
+3. 아이템 삭제 버튼을 누르면 삭제가 가능하다.
+ */
 function App() {
+  const [inputValue, setInputValue] = useState('');
+  const [todoList, setTodoList] = useState([]);
+  const addItem = ()=>{
+    console.log("im here" + inputValue);
+    setTodoList([...todoList, inputValue]);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <main>
+        <input type="text" onChange={(event)=>setInputValue(event.target.value)}/>
+        <button onClick={addItem}>추가</button>
+        <TodoBoard todoList={todoList}/>
+      </main>
   );
 }
 
